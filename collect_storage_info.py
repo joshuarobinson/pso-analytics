adecba90fed0de4b42372041bc65450994d82de8
Joshua Robinson <joshua@purestorage.com>
2020-05-31 06:54:15 -0700

    Add poll-seconds as command line option and internal refactor.
    Remove the shell infinite loop in yaml and implement instead in python.
    Internal refactor to make data collection a separate function.
    
    Preparation for Prometheus support.

--- collect_storage_info.py
+++ collect_storage_info.py
@@ -9,6 +9,7 @@ import copy
 import json
 import os
 import re
+import time
 import urllib3
 
 
@@ -51,81 +52,123 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--output', choices=['table', 'json'],
                     default='table',
                     help='Output format.')
+parser.add_argument('--poll-seconds', default=3600, type=int, help='Frequency to poll stats')
 args = parser.parse_args()
 
-#========= Login to Kubernetes cluster ======================
+def collect_volumes():
+    #========= Login to Kubernetes cluster ======================
 
-# Configs can be set in Configuration class directly or using helper utility
-kubernetes.config.load_incluster_config()
+    # Configs can be set in Configuration class directly or using helper utility
+    kubernetes.config.load_incluster_config()
 
-v1 = kubernetes.client.CoreV1Api()
+    v1 = kubernetes.client.CoreV1Api()
 
-# Collect state about each PVC found in the system.
-pvcs = {}
-ret = v1.list_persistent_volume_claim_for_all_namespaces(watch=False)
-for i in ret.items:
-    pvcs[i.metadata.uid] = {"name": i.metadata.name, "namespace":
-        i.metadata.namespace, "storageclass": i.spec.storage_class_name,
-        "labels": i.metadata.labels}
+    # Collect state about each PVC found in the system.
+    pvcs = {}
+    ret = v1.list_persistent_volume_claim_for_all_namespaces(watch=False)
+    for i in ret.items:
+        pvcs[i.metadata.uid] = {"name": i.metadata.name, "namespace":
+            i.metadata.namespace, "storageclass": i.spec.storage_class_name,
+            "labels": i.metadata.labels}
 
-# To group PVCs by StatefulSet, create regexes that matches the naming
-# convention for the PVCs that belong to VolumeClaimTemplates.
-ss_regexes = {}
-ret = kubernetes.client.AppsV1Api().list_stateful_set_for_all_namespaces(watch=False)
-for i in ret.items:
-    if i.spec.volume_claim_templates:
-        for vct in i.spec.volume_claim_templates:
-            ssname = i.metadata.name + "." + i.metadata.namespace
-            ss_regexes[ssname] = re.compile(vct.metadata.name + "-" + i.metadata.name + "-[0-9]+")
+    # To group PVCs by StatefulSet, create regexes that matches the naming
+    # convention for the PVCs that belong to VolumeClaimTemplates.
+    ss_regexes = {}
+    ret = kubernetes.client.AppsV1Api().list_stateful_set_for_all_namespaces(watch=False)
+    for i in ret.items:
+        if i.spec.volume_claim_templates:
+            for vct in i.spec.volume_claim_templates:
+                ssname = i.metadata.name + "." + i.metadata.namespace
+                ss_regexes[ssname] = re.compile(vct.metadata.name + "-" + i.metadata.name + "-[0-9]+")
 
-# Search for PURE_K8S_NAMESPACE
-pso_namespace = ""
-pso_prefix = ""
-ret = v1.list_pod_for_all_namespaces(watch=False)
-for i in ret.items:
-    for c in i.spec.containers:
-        if c.env:
-            for e in c.env:
-                if e.name == "PURE_K8S_NAMESPACE":
-                    pso_prefix = e.value
-                    pso_namespace = i.metadata.namespace
-                    break
+    # Search for PURE_K8S_NAMESPACE
+    pso_namespace = ""
+    pso_prefix = ""
+    ret = v1.list_pod_for_all_namespaces(watch=False)
+    for i in ret.items:
+        for c in i.spec.containers:
+            if c.env:
+                for e in c.env:
+                    if e.name == "PURE_K8S_NAMESPACE":
+                        pso_prefix = e.value
+                        pso_namespace = i.metadata.namespace
+                        break
 
-if not pso_namespace:
-    print("Did not find PSO, exiting")
-    exit(1)
+    if not pso_namespace:
+        print("Did not find PSO, exiting")
+        exit(1)
 
-# Find the secret associated with the pure-provisioner in order to find the
-# login info for all FlashArrays and FlashBlades.
-flashblades={}
-flasharrays={}
+    # Find the secret associated with the pure-provisioner in order to find the
+    # login info for all FlashArrays and FlashBlades.
+    flashblades={}
+    flasharrays={}
 
-secrets = v1.read_namespaced_secret("pure-provisioner-secret", pso_namespace)
-rawbytes = base64.b64decode(secrets.data['pure.json'])
-purejson = json.loads(rawbytes.decode("utf-8"))
-pso_namespace = i.metadata.namespace
-flashblades = purejson["FlashBlades"] if "FlashBlades" in purejson else {}
-flasharrays = purejson["FlashArrays"] if "FlashArrays" in purejson else {}
+    secrets = v1.read_namespaced_secret("pure-provisioner-secret", pso_namespace)
+    rawbytes = base64.b64decode(secrets.data['pure.json'])
+    purejson = json.loads(rawbytes.decode("utf-8"))
+    pso_namespace = i.metadata.namespace
+    flashblades = purejson["FlashBlades"] if "FlashBlades" in purejson else {}
+    flasharrays = purejson["FlashArrays"] if "FlashArrays" in purejson else {}
 
 
-# Begin collecting and correlating volume information from the backends.
-vols = []
+    # Begin collecting and correlating volume information from the backends.
+    vols = []
 
-# Disable warnings due to unsigned SSL certs.
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    # Disable warnings due to unsigned SSL certs.
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-#========= Login to FlashArrays ======================
+    #========= Login to FlashArrays ======================
 
-for fajson in flasharrays:
-    fa = purestorage.FlashArray(fajson["MgmtEndPoint"], api_token=fajson["APIToken"])
+    for fajson in flasharrays:
+        fa = purestorage.FlashArray(fajson["MgmtEndPoint"], api_token=fajson["APIToken"])
 
-    try:
-        for vol in fa.list_volumes(names=[pso_prefix + "*"], space=True):
-            assert vol["name"].startswith(pso_prefix + "-pvc-")
-            uid = vol["name"].replace(pso_prefix + "-pvc-", "")
+        try:
+            for vol in fa.list_volumes(names=[pso_prefix + "*"], space=True):
+                assert vol["name"].startswith(pso_prefix + "-pvc-")
+                uid = vol["name"].replace(pso_prefix + "-pvc-", "")
+
+                if uid not in pvcs:
+                    print("Found orphan PersistentVolume: " + uid + " on FlashArray " + fajson["MgmtEndPoint"])
+                    continue
+
+                pvc = pvcs[uid]
+
+                tags = {"all": "all",
+                        "storageclass": pvc["storageclass"],
+                        "namespace": pvc["namespace"],
+                        "backend": "FA " + fajson["MgmtEndPoint"]}
+                if pvc["labels"]:
+                    for l in pvc["labels"]:
+                        tags["label/" + l] = pvc["labels"][l]
+                for ssname,rgx in ss_regexes.items():
+                    if rgx.match(pvc["name"]):
+                        tags["statefulset"] = ssname
+
+                vol = {"uid": uid,
+                       "logical_bytes": vol["total"],
+                       "physical_bytes": vol["total"] / vol["data_reduction"],
+                       "provisioned_bytes": vol["size"],
+                       "tags": tags}
+
+                vols.append(vol)
+
+        except:
+            pass
+
+
+    #========= Login to FlashBlades ======================
+
+    for fbjson in flashblades:
+        fb = purity_fb.PurityFb(fbjson["MgmtEndPoint"],
+                                api_token=fbjson["APIToken"])
+
+        res = fb.file_systems.list_file_systems(filter="name='" + pso_prefix + "*'")
+        for fs in res.items:
+            assert fs.name.startswith(pso_prefix + "-pvc-")
+            uid = fs.name.replace(pso_prefix + "-pvc-", "")
 
             if uid not in pvcs:
-                print("Found orphan PersistentVolume: " + uid + " on FlashArray " + fajson["MgmtEndPoint"])
+                print("Found orphan PersistentVolume: " + uid + " on FlashBlade " + fbjson["MgmtEndPoint"])
                 continue
 
             pvc = pvcs[uid]
@@ -133,7 +176,7 @@ for fajson in flasharrays:
             tags = {"all": "all",
                     "storageclass": pvc["storageclass"],
                     "namespace": pvc["namespace"],
-                    "backend": "FA " + fajson["MgmtEndPoint"]}
+                    "backend": "FB " + fbjson["MgmtEndPoint"]}
             if pvc["labels"]:
                 for l in pvc["labels"]:
                     tags["label/" + l] = pvc["labels"][l]
@@ -142,75 +185,44 @@ for fajson in flasharrays:
                     tags["statefulset"] = ssname
 
             vol = {"uid": uid,
-                   "logical_bytes": vol["total"],
-                   "physical_bytes": vol["total"] / vol["data_reduction"],
-                   "provisioned_bytes": vol["size"],
+                   "logical_bytes": fs.space.virtual,
+                   "physical_bytes": fs.space.total_physical,
+                   "provisioned_bytes": fs.provisioned,
                    "tags": tags}
 
             vols.append(vol)
 
-    except:
-        pass
-
-
-#========= Login to FlashBlades ======================
-
-for fbjson in flashblades:
-    fb = purity_fb.PurityFb(fbjson["MgmtEndPoint"],
-                            api_token=fbjson["APIToken"])
-
-    res = fb.file_systems.list_file_systems(filter="name='" + pso_prefix + "*'")
-    for fs in res.items:
-        assert fs.name.startswith(pso_prefix + "-pvc-")
-        uid = fs.name.replace(pso_prefix + "-pvc-", "")
-
-        if uid not in pvcs:
-            print("Found orphan PersistentVolume: " + uid + " on FlashBlade " + fbjson["MgmtEndPoint"])
-            continue
-
-        pvc = pvcs[uid]
-
-        tags = {"all": "all",
-                "storageclass": pvc["storageclass"],
-                "namespace": pvc["namespace"],
-                "backend": "FB " + fbjson["MgmtEndPoint"]}
-        if pvc["labels"]:
-            for l in pvc["labels"]:
-                tags["label/" + l] = pvc["labels"][l]
-        for ssname,rgx in ss_regexes.items():
-            if rgx.match(pvc["name"]):
-                tags["statefulset"] = ssname
-
-        vol = {"uid": uid,
-               "logical_bytes": fs.space.virtual,
-               "physical_bytes": fs.space.total_physical,
-               "provisioned_bytes": fs.provisioned,
-               "tags": tags}
-
-        vols.append(vol)
+    return vols
 
 #========= Print out Results ======================
 
-if args.output == 'table':
-    # Grab the unique list of keys in the tags across all volumes.
-    tablenames = list(set([y for x in [list(v["tags"].keys()) for v in vols] for y in x]))
+while True:
+    vols = collect_volumes()
 
-    for tab in tablenames:
-        thistab = {}
-        print("\n==== {} =====".format(tab))
+    if args.output == 'table':
+        # Grab the unique list of keys in the tags across all volumes.
+        tablenames = list(set([y for x in [list(v["tags"].keys()) for v in vols] for y in x]))
+
+        for tab in tablenames:
+            thistab = {}
+            print("\n==== {} =====".format(tab))
+            for v in vols:
+                if tab in v["tags"]:
+                    key = v["tags"][tab]
+                    newrow = {"logical_bytes": v["logical_bytes"],
+                              "physical_bytes": v["physical_bytes"],
+                              "provisioned_bytes": v["provisioned_bytes"],
+                              "volume_count": 1}
+                    thistab[key] = sum_volume_records(thistab[key], newrow) if key in thistab else copy.copy(newrow)
+
+            # Flatten to a list and put the "name" back into to the dict (for tabulate)
+            finaltab = [{"name": k, **prettify_record(thistab[k])} for k in thistab.keys()]
+            print(tabulate.tabulate(finaltab, headers="keys"))
+
+    elif args.output == 'json':
         for v in vols:
-            if tab in v["tags"]:
-                key = v["tags"][tab]
-                newrow = {"logical_bytes": v["logical_bytes"],
-                          "physical_bytes": v["physical_bytes"],
-                          "provisioned_bytes": v["provisioned_bytes"],
-                          "volume_count": 1}
-                thistab[key] = sum_volume_records(thistab[key], newrow) if key in thistab else copy.copy(newrow)
+            print(v)
 
-        # Flatten to a list and put the "name" back into to the dict (for tabulate)
-        finaltab = [{"name": k, **prettify_record(thistab[k])} for k in thistab.keys()]
-        print(tabulate.tabulate(finaltab, headers="keys"))
+    print("", flush=True)
 
-elif args.output == 'json':
-    for v in vols:
-        print(v)
+    time.sleep(args.poll_seconds)
